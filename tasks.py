fdd5abc445e2268c435ed2c02423a9ffd5ac19a0
Jack Muratore <jackmuratore@gmail.com>
2016-08-16 21:46:25 -0400

    Deploy to giantgreendinosaur

---
+++ tasks.py
@@ -0,0 +1,19 @@
+from invoke import task
+
+
+@task
+def deploy(ctx):
+    '''
+    Deploys to staging
+    '''
+    ctx.run(
+            'ansible-playbook -i '
+            './ansible/inv.ini '
+            '-u root '
+            '--tags "deploy" '
+            '--extra-vars="'
+            'docker_user=$DOCKER_USER '
+            'docker_password=$DOCKER_PASS '
+            'docker_email=$DOCKER_EMAIL" '
+            './ansible/playbook.yml')
+
